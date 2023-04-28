1e9e43adfefa95ba1063499299925dabf4cc6313
matkad92 <mateuszkadziolka92@gmail.com>
2023-04-28 14:55:12 +0200

    Employee Finder finished

--- BTEtask/BTEtask/BTEtask.cpp
+++ BTEtask/BTEtask/BTEtask.cpp
@@ -4,11 +4,11 @@
 
 int main()
 {
+    int seekedId{ 5 };
+    
     CurlRequester myRequest;
-    EmployeeFinder myEmployee;
 
-    std::string test = myRequest.getAllEmployees();
-    std::cout << test<<std::endl;
-    std::cout << "Program end\n";
+    EmployeeFinder myEmployee(seekedId, myRequest.getAllEmployees());
+    
 }
 
--- BTEtask/BTEtask/EmployeeFinder.h
+++ BTEtask/BTEtask/EmployeeFinder.h
@@ -1,5 +1,26 @@
 #pragma once
+#include <string>
+#include "json.hpp"
+#include <iostream>
+
+using json = nlohmann::json;
+
 class EmployeeFinder
 {
+	int seekedId;
+	std::string firstName, lastName;
+	void findEmployee(const std::string& list);
+	void printEmployee();
+
+public:
+	EmployeeFinder(int sendId, const std::string &list )
+	{
+		seekedId = sendId;
+		findEmployee(list);
+		printEmployee();
+	
+	}
+
+
 };
 
